96b1e04d5a67661877588cb774ca37f5ce5d3fda
Carlos Andrés Pavas Correa <pavas0921@gmail.com>
2024-07-21 18:42:52 -0500

    actualizacion

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  // // Habilitar CORS
+  // app.enableCors({
+  //   origin: 'http://localhost:5173',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
 
   app.useGlobalPipes(
     new ValidationPipe({
